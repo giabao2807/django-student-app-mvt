95ae4019012095917fef8e36529a68d46c09eb7b
GiaBaone <102190252@sv1.dut.udn.vn>
2022-04-19 00:31:15 +0700

    form in django

--- api_student/forms.py
+++ api_student/forms.py
@@ -1,7 +1,59 @@
 from django import forms
 from .models import Student
 
+
 class StudentForms(forms.ModelForm):
+    code = forms.CharField(
+        label='MSSV',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'place-holder': 'MSSV'
+        }))
+    name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'place-holder': 'Fullname'
+        })
+    )
+    address = forms.CharField(
+        initial='Viet Nam',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'rows': 10,
+            'cols': 10
+        })
+    )
+
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = ['code', 'name', 'address']
+
+    def clean_code(self, *args, **kwargs):
+        code = self.cleaned_data.get('code')
+        if not code.isnumeric():
+            raise forms.ValidationError("The code should be digit only")
+        return code
+
+
+class RawStudentForms(forms.Form):
+    code = forms.CharField(
+        label='MSSV',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'place-holder': 'MSSV'
+        }))
+    name = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'place-holder': 'Fullname'
+        })
+    )
+    address = forms.CharField(
+        initial='Viet Nam',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'rows': 10
+        })
+    )
--- api_student/views.py
+++ api_student/views.py
@@ -3,26 +3,45 @@ from django.shortcuts import render
 
 
 # Create your views here.
-from api_student.forms import StudentForms
+from api_student.forms import StudentForms, RawStudentForms
 from api_student.models import Student
 
-def create_view(request):
-    if request.method =='post':
-        new_code = request.POST.get('code')
-        print(new_code)
-    context = {
-
-    }
-    return render(request, 'create.html', context)
 
 # def create_view(request):
-#     form = StudentForms
-#     if form.is_valid():
-#         form.save()
+#     form = RawStudentForms()
+#
+#     if request.method:
+#         form = RawStudentForms(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             Student.objects.create(**form.cleaned_data)
+#         else:
+#             print(form.errors)
+#
 #     context = {
 #         'form': form
 #     }
 #     return render(request, 'create.html', context)
+
+# def create_view(request):
+#     if request.method =='post':
+#         new_code = request.POST.get('code')
+#         print(new_code)
+#     context = {
+#
+#     }
+#     return render(request, 'create.html', context)
+
+def create_view(request):
+    form = StudentForms(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form =StudentForms()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'create.html', context)
 
 def detail_view(request):
     student = Student.objects.filter(id=1).first()
