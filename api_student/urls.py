0df80f00e4f2a3491811e2f622e5884a6ece071b
GiaBaone <102190252@sv1.dut.udn.vn>
2022-04-18 13:39:29 +0700

    form models base

--- api_student/urls.py
+++ api_student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import detail_view
+from .views import detail_view, create_view
 
 urlpatterns = [
-    path('detail', detail_view, {})
+    path('detail', detail_view, {}),
+    path('create', create_view, {})
 ]
--- api_student/views.py
+++ api_student/views.py
@@ -3,8 +3,26 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from api_student.forms import StudentForms
 from api_student.models import Student
 
+def create_view(request):
+    if request.method =='post':
+        new_code = request.POST.get('code')
+        print(new_code)
+    context = {
+
+    }
+    return render(request, 'create.html', context)
+
+# def create_view(request):
+#     form = StudentForms
+#     if form.is_valid():
+#         form.save()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'create.html', context)
 
 def detail_view(request):
     student = Student.objects.filter(id=1).first()
